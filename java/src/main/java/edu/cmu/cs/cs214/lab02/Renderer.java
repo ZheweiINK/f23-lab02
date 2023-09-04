bb7ad57946a494471fbb75a83021bb1f20ac9a3c
klaustzw <klaustzw@gmail.com>
2023-09-04 17:18:49 -0400

    java encapsulation improved

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
@@ -3,7 +3,7 @@ package edu.cmu.cs.cs214.lab02;
 import edu.cmu.cs.cs214.lab02.shapes.*;
 
 public class Renderer {
-    public Rectangle rectangle;
+    private Rectangle rectangle;
     
     Renderer(Rectangle rectangle) {
         this.rectangle = rectangle;
@@ -11,7 +11,7 @@ public class Renderer {
 
     void draw() {
         double area = rectangle.getArea();
-
+        
         // assume implementation
 
         System.out.println("Shape printed\n" + "Its area is " + area);
